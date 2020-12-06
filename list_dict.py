eeeaf22890df753b52781470dd8a04c66ba7c648
Jackson Chang <45678100+DEFICITT@users.noreply.github.com>
2020-12-06 23:32:26 +0800

    字典和列表的学习（未完成1）

--- list_dict.py
+++ list_dict.py
@@ -1,10 +1,34 @@
-a = []
-b = ['a', 'b', 'c', 'd']
+# a = []
+# b = ['a', 'b', 'c', 'd']
 # print(a)
 # print(b)
 # print(type(a))
 # print(type(b))
 
+# for index, value in enumerate(b):
+#     print(index, value)
+
+# # 列表长度
+# print(len(b))
+#
+# # 列表通过下标访问元素
+# print(b[2])
+#
+# # 列表切片操作
+# print(b[0:2])
+
+# # 列表元素查找
+# print(b.index('a'))
+
+# # 列表排序
+# list2 = ['1', '9', '4', '5', '8']
+# # # a = sorted(list2)
+# # # print(a)
+# # list2.sort()
+# # print(list2)
+#
+# list2.reverse()
+# print(list2)
 for index, value in enumerate(b):
     print(value)
     print(index)
